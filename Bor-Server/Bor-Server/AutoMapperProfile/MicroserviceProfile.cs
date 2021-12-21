2feb275b0749b170ebc8b9e6df48af58e173f325
Diana001a <di89101630730@mail.ru>
2021-12-21 19:21:37 +0300

    fhfhhf
    fhfhfyh

---
+++ Bor-Server/Bor-Server/AutoMapperProfile/MicroserviceProfile.cs
@@ -0,0 +1,20 @@
+﻿using Albina.BusinesLogic.Core.Models;
+using AutoMapper;
+using Bor.Core.Models;
+
+
+
+namespace Bor_Server.AutoMapperProfile
+{
+    public class MicroserviceProfile : Profile
+    {
+        public MicroserviceProfile()
+        {
+            CreateMap<UserInformationBlo, UserInfoprmationDto>();
+            CreateMap<UserIdentityBlo, UserIdentityDto>();
+            CreateMap<UserIdentityDto, UserIdentityBlo>();
+            CreateMap<UserUpdateDto, UserUpdateBlo>();
+            CreateMap<UserUpdateBlo, UserUpdateDto>();
+        }
+    }
+}
---
+++ Bor-Server/Bor-Server/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bor.DataAccess.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bor.Core.Models;
+using Albina.BusinesLogic.Core.Interfaces;
+using AutoMapper;
+using Albina.BusinesLogic.Core.Models;
+
+namespace Bor_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserService _userService;
+        public UserController(IMapper mapper, IUserService userService)
+        {
+            _mapper = mapper;
+            _userService = userService;
+        }
+        [HttpPost("/auth")]
+        public async Task<ActionResult<UserInfoprmationDto>> Auth([FromBody]UserIdentityDto userIdentityDto)
+        {
+            UserIdentityBlo userIdentityBlo = _mapper.Map<UserIdentityBlo>(userIdentityDto);
+            UserInformationBlo userInformationBlo = await _userService.Auth(userIdentityBlo);
+            UserInfoprmationDto userInformationDto = _mapper.Map<UserInfoprmationDto>(userInformationBlo);
+            return userInformationDto;
+        }
+        [HttpPost("/register")]
+        public async Task<ActionResult<UserInfoprmationDto>> Register([FromBody]UserIdentityDto userIdentityDto)
+        {
+            UserIdentityBlo userIdentityBlo = _mapper.Map<UserIdentityBlo>(userIdentityDto);
+            UserInformationBlo userInformationBlo = await _userService.Register(userIdentityBlo);
+            UserInfoprmationDto userInformationDto = _mapper.Map<UserInfoprmationDto>(userInformationBlo);
+            return userInformationDto;
+        }
+        [HttpPost("/update")]
+        public async Task<ActionResult<UserInfoprmationDto>> Update([FromBody] UserIdentityDto userIdentityDto)
+        {
+           
+        }
+    }
+}
